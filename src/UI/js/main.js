6c74a5b6b70dc7f2bfa0110508d9a98d38754435
ArwaShamardal <76781020+ArwaShamardal@users.noreply.github.com>
2022-05-13 19:30:55 +0200

    updated search query in search_results page

--- src/UI/js/main.js
+++ src/UI/js/main.js
@@ -1,20 +1,26 @@
 
+//function that takes query input and redirects to search results page
+//Uses local storage to store/update the search query
+function searchByQuery(queryInput,searchQuery){
+    // Execute a function when the user presses a key on the keyboard
+    queryInput.addEventListener("keypress", function(event) {
+        if (event.key === "Enter") {
+            // Get the value of the input field
+            searchQuery = queryInput.value;
+            event.preventDefault();
+            console.log("search query is " + searchQuery);
+
+            localStorage.setItem("SEARCHQUERY",searchQuery);
+            window.open("search_results.html", "_self");
+        }
+    });
+}
+
 let queryInput = document.getElementById("search-query-id");
 let searchQuery;
 
+searchByQuery(queryInput,searchQuery);
 
-// Execute a function when the user presses a key on the keyboard
-queryInput.addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-        // Get the value of the input field
-        searchQuery = queryInput.value;
-        event.preventDefault();
-        console.log("search query is " + searchQuery);
-
-        localStorage.setItem("SEARCHQUERY",searchQuery);
-        window.open("search_results.html", "_self");
-    }
-});
 
 
 
