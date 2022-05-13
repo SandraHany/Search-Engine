1c941727ed739aafe575f99ee8dfa95a96a7775b
ArwaShamardal <76781020+ArwaShamardal@users.noreply.github.com>
2022-05-14 01:52:22 +0200

    Added pagination using page numbers

--- src/UI/js/search_results.js
+++ src/UI/js/search_results.js
@@ -46,34 +46,63 @@ queryInput.addEventListener("keypress", function(event) {
 
 
 
-let websitesPerPage=10;
+let maxWebsitesPerPage=10;
 let currentPage=1;
 let mainWebsiteDivision = document.getElementsByClassName("websites-container-class");
 let paginationDivision = document.getElementsByClassName("pagination-class");
 
 
-function displayWebsites(linksList, wrapper, websitesPerPage, currentPage) {
+function createWebsite(item){
+    let websiteDivision = document.createElement("div");
+    let textNode = document.createTextNode(item);
+    let textNode1 = document.createTextNode("3aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+    let link = document.createElement("a");
+    websiteDivision.className = "website-division-class";
+    link.href = item;
+    link.appendChild(textNode);
+    websiteDivision.appendChild(link);
+    websiteDivision.appendChild(textNode1);
+    mainWebsiteDivision[0].appendChild(websiteDivision);
+}
+
+function displayWebsites(linksList, wrapper, maxWebsitesPerPage, currentPage) {
     wrapper.innerHTML= "";
     currentPage--;
-    let start = currentPage*websitesPerPage;
-    let end = start + websitesPerPage;
+    let start = currentPage*maxWebsitesPerPage;
+    let end = start + maxWebsitesPerPage;
     let listItems = linksList.slice(start, end);
     for (let i = 0; i < listItems.length; i++) {
-        let websiteDivision = document.createElement("div");
-        let textNode = document.createTextNode(listItems[i]);
-        let textNode1 = document.createTextNode("3aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-        let link = document.createElement("a");
-        websiteDivision.className = "website-division-class";
-        link.href = listItems[i];
-        link.appendChild(textNode);
-        websiteDivision.appendChild(link);
-        websiteDivision.appendChild(textNode1);
-        mainWebsiteDivision[0].appendChild(websiteDivision);
+        createWebsite(listItems[i]);
+    }
+}
+var flag = true;
+
+function pagenation(linksList, wrapper, maxWebsitesPerPage){
+    wrapper.innerHTML= "";
+    let pageCount = Math.ceil(linksList.length/maxWebsitesPerPage);
+    for (let i = 1; i < pageCount+1; i++) {
+        let button = document.createElement("button");
+        button.className = "pagination-button-class";
+        button.innerText = i;
+        if(currentPage==i){
+            button.classList.add("active");
+        }
+        button.addEventListener("click", function(){
+            currentPage = i;
+            displayWebsites(linksList, mainWebsiteDivision[0], maxWebsitesPerPage, currentPage);
+    
+        });
+        wrapper.appendChild(button);
+
+
     }
 }
 
-displayWebsites(linksList, mainWebsiteDivision[0], websitesPerPage, currentPage);
 
+
+displayWebsites(linksList, mainWebsiteDivision[0], maxWebsitesPerPage, currentPage);
+pagenation(linksList, paginationDivision[0], maxWebsitesPerPage);
+console.log("heellloo");
 
 
 
