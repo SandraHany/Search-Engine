9db3bb87884c8efe86d289cb85f3ccf4e54bc13d
SandraHany <sandra.nasry00@eng-st.cu.edu.eg>
2022-05-19 05:43:53 +0200

    final indexer

--- src/main/java/net/codejava/MongoDBClass.java
+++ src/main/java/net/codejava/MongoDBClass.java
@@ -3,6 +3,7 @@ package net.codejava;
 import org.bson.Document;
 
 import com.mongodb.BasicDBObject;
+import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
@@ -10,27 +11,25 @@ import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 
 public class MongoDBClass {
+		private MongoClient mongoClientIndexer;
+		private MongoDatabase dbIndexer;
+		private MongoCollection indexerCollection; 
+		private MongoCollection indexedLinksCollection; 
+   
 	
-		private MongoClient mongoClient;
-	    private MongoDatabase db; 
 	    public MongoDBClass() {
-			
-		this. mongoClient = MongoClients.create("mongodb+srv://Sandra:fmCs6CAZx0phSrjs@cluster0.real4.mongodb.net/SearchEngine?retryWrites=true&w=majority");
-	    this. db = mongoClient.getDatabase("SearchEngine");		
-			
+	    	//2 clusters to avoid limited free space quota
+	    	this.mongoClientIndexer= MongoClients.create("mongodb://Sandra:fmCs6CAZx0phSrjs@cluster0-shard-00-00.real4.mongodb.net:27017,cluster0-shard-00-01.real4.mongodb.net:27017,cluster0-shard-00-02.real4.mongodb.net:27017/SearchEngine?ssl=true&replicaSet=atlas-bhl30t-shard-0&authSource=admin&retryWrites=true&w=majority");
+	        this.dbIndexer = mongoClientIndexer.getDatabase("SearchEngine");	       
+	        this.indexerCollection=dbIndexer.getCollection("indexers"); //contains inverted index
+	        this.indexedLinksCollection=dbIndexer.getCollection("indexedlinks"); //contains previously indexed  links to avoid indexing them again
 	    }
-	    public MongoCollection<Document> DBgetCollection(String CollectionName) {
-	    	return db.getCollection(CollectionName);
+	    public MongoCollection getIndexerCollection() {
+	    	return indexerCollection;
 	    }
-	    boolean wordExists(String word) {
-	    	MongoCursor<org.bson.Document> cursor=null;  
-	    	BasicDBObject  criteria = new BasicDBObject();
-			criteria.append("word",word);
-			cursor = DBgetCollection("Indexer").find(criteria).cursor();  
-			 if(!cursor.hasNext()) {
-				 return false;
-			 }
-			 return true;
+	    public MongoCollection getIndexedLinksCollection() {
+	    	return indexedLinksCollection;
 	    }
+	    
 	
 }
